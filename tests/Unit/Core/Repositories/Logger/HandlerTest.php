9547e2a3226206baa3e2e207361f4dfa1c866e1c
Lucas Block <lucas.blockv@gmail.com>
2023-05-02 10:29:22 -0300

    feat: add ci testes

---
+++ tests/Unit/Core/Repositories/Logger/HandlerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Core\Repositories\Logger\Handler;
+use PHPUnit\Framework\TestCase;
+
+class HandlerTest extends TestCase
+{
+    public function testIsHandling()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testHandle()
+    {
+        $this->assertTrue(true);
+    }
+}
